8259458259e3a876847287b20613732d777caa4e
changhwan <changhwan.park@balaan.co.kr>
2022-10-01 10:46:54 +0900

    ADD boardController

---
+++ src/main/java/com/example/chuchu/board/controller/BoardController.java
@@ -0,0 +1,11 @@
+package com.example.chuchu.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+
+}
---
+++ src/main/java/com/example/chuchu/board/dto/BoardDTO.java
@@ -0,0 +1,7 @@
+package com.example.chuchu.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardDTO {
+}
