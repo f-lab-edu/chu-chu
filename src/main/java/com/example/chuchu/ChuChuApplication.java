b5747a48eaca2844ebff1c0e1a52b070ee246e68
changhwan <changhwan.park@balaan.co.kr>
2022-10-01 10:28:42 +0900

    spring 프로젝트 생성

---
+++ src/main/java/com/example/chuchu/ChuChuApplication.java
@@ -0,0 +1,13 @@
+package com.example.chuchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuChuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuChuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chuchu/ChuChuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chuchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuChuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
