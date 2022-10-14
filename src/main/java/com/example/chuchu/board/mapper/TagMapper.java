6ac0d95a0ad14b6df10a34706aecb67d31a5eef3
changhwan <changhwan.park@balaan.co.kr>
2022-10-15 00:52:14 +0900

    [#19] TagDTO 생성

---
+++ src/main/java/com/example/chuchu/board/mapper/TagMapper.java
@@ -0,0 +1,14 @@
+package com.example.chuchu.board.mapper;
+
+import com.example.chuchu.board.dto.BoardDTO;
+import com.example.chuchu.board.dto.TagDTO;
+import com.example.chuchu.board.entity.Board;
+import com.example.chuchu.board.entity.Tag;
+import com.example.chuchu.common.global.GenericMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface TagMapper extends GenericMapper<TagDTO, Tag> {
+    TagMapper INSTANCE = Mappers.getMapper(TagMapper.class);
+}
