4a93ffbe3bc27e9ab1bd7f17ccdd6534654ba190
yht0827 <yangheetae0827@naver.com>
2022-10-29 15:43:19 +0900

    [#29] JASYPT를 사용하여 프로퍼티 암호화 처리
    - jasypt 라이브러리 추가 및 환경 설정 작업
    - 메일 및 jwt 시크릿 키 암호화 처리

---
+++ src/main/java/com/example/chuchu/common/config/JasyptConfig.java
@@ -0,0 +1,30 @@
+package com.example.chuchu.common.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setPoolSize("1");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setStringOutputType("base64");
+        config.setKeyObtentionIterations("1000");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
--- src/main/java/com/example/chuchu/common/config/MailConfig.java
+++ src/main/java/com/example/chuchu/common/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.example.chuchu.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,11 +11,17 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.username}")
+    private String email;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setUsername(""); // 계정 아이디
-        javaMailSender.setPassword(""); // 계정 비밀번호
+        javaMailSender.setUsername(email); // 계정 아이디
+        javaMailSender.setPassword(password); // 계정 비밀번호
         javaMailSender.setHost("smtp.naver.com"); // smtp 서버 주소
         javaMailSender.setPort(465); // 메일 인증서버 포트
         javaMailSender.setJavaMailProperties(getProperties()); // 메일 인증서버 정보
